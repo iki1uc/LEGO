e663d8b7ce61acd9461b26dd1712f97d0464e9dd
iki1uc <entertrainer@web.de>
2026-07-14 04:17:08 +0200

    Refactor core.js to export constants and structures

--- core.js
+++ core.js
@@ -1,8 +1,35 @@
-698869 – Engine‑Stations‑System
+// core.js – 698869 Stations-System (RAW, gültig)
 
-POS (6)
-WECHSEL (9)
-T (8)
-GO (8)
-LE (6)
-Z (9)
+export const STATION = {
+    POS: 6,        // Position
+    WECHSEL: 9,    // Wechsel
+    T: 8,          // Zeit
+    GO: 8,         // Aktivierung
+    LE: 6,         // Abschluss
+    Z: 9           // Ergebnis
+};
+
+export const PIPELINE = [
+    "POS",
+    "WECHSEL",
+    "T",
+    "GO",
+    "LE",
+    "Z"
+];
+
+export const CACHE = {
+    pos: null,
+    wechsel: null,
+    t: null,
+    go: null,
+    le: null,
+    z: null
+};
+
+export const BENCH = {
+    POS: [],
+    WECHSEL: [],
+    T: [],
+    Z: []
+};
